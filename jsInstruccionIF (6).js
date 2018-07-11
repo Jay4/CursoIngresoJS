a186b261ba26f9fa5d8f656918aa7d6dca4e940f
Jay4 <40810417+Jay4@users.noreply.github.com>
2018-07-10 21:57:23 -0300

    If

---
+++ jsInstruccionIF (6).js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+//tomo la edad  
+   var edad;
+   edad=document.getElementById('edad').value;
+
+   if (edad < 13) 
+   {
+   	    alert("es menor")
+   }else
+
+
+
+}//FIN DE LA FUNCIÓN
