b08b029985d6362c3b1d9de0969fdef71edfdd54
Jay4 <40810417+Jay4@users.noreply.github.com>
2018-07-10 22:14:36 -0300

    if revisar

---
+++ jsInstruccionIF (8).js
@@ -0,0 +1,25 @@
+function mostrar()
+{
+//tomo la edad  
+    var edad;
+    var estcivil;
+    edad=document.getElementById('edad').value;
+    estcivil=document.getElementById('estadoCivil').value;
+
+    if (edad<18 && estcivil!=Soltero) 
+    {
+
+    }else
+    {
+    	alert("es soltero y no es menor");
+    }
+
+
+
+
+
+
+	
+
+
+}//FIN DE LA FUNCIÓN
